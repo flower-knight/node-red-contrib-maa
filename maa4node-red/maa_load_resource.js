f563050ec5ee8b1ddb3953e49d3fe3b5ecc2f6e9
白华棠君 <lhn675699635@gmail.com>
2023-08-09 03:37:32 +0800

    完善load resource节点

--- maa4node-red/maa_load_resource.js
+++ maa4node-red/maa_load_resource.js
@@ -1,8 +1,21 @@
 module.exports = function (RED) {
     function LoadResourceNode(config) {
+        RED.nodes.createNode(this, config);
+        let maaPath = config.maaPath
+        let node = this;
 
+        node.on("input", function (msg, send, done) {
+            send = send || function () {
+                node.send.apply(node, arguments)
+            };
+            msg.payload = maaPath;
+            send(msg);
+            // status = { fill: "green", shape: "dot", text: RED._("mysql.status.ok") };
+            // node.status(status);
+        })
     }
-    RED.nodes.registerType("load_resource", LoadResourceNode,{
+
+    RED.nodes.registerType("load_resource", LoadResourceNode, {
         defaults: {
             maaPath: {value: ""},
         }
